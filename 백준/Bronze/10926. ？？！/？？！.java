a5336934ba1b4a5f870bfdf1e588b4441309974f
Seyeon Oh <117005656+oosedus@users.noreply.github.com>
2024-03-13 00:49:35 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        String answer = input.concat("??!");
+
+        System.out.println(answer);
+    }
+}
