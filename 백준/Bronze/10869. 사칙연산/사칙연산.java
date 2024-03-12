4af68dcf2fccebfd8fe368723f96cccd134bd0b3
Seyeon Oh <117005656+oosedus@users.noreply.github.com>
2024-03-13 00:47:40 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(" ");
+
+        int number_1 = Integer.parseInt(input[0]);
+        int number_2 = Integer.parseInt(input[1]);
+
+        System.out.println(number_1 + number_2);
+        System.out.println(number_1 - number_2);
+        System.out.println(number_1 * number_2);
+        System.out.println(number_1 / number_2);
+        System.out.println(number_1 % number_2);
+    }
+}
