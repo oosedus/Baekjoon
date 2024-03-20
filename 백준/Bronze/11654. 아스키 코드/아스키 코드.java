16b5b39c3450970482cee49e850dc00ba23a0174
Seyeon Oh <117005656+oosedus@users.noreply.github.com>
2024-03-20 14:57:31 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char input = br.readLine().charAt(0);
+
+        int answer = input;
+        
+        System.out.println(answer);
+    }
+}
